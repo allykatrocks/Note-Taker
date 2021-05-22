21da01dc674178197e623377cbc7da839fe1ff52
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-05-22 14:34:01 -0700

    changed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const { stringify } = require('querystring');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
